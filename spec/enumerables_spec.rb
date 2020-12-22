7c1d3e0879f2d9a291094cfc7764da40a4a71c24
oluwadare juwon <wintan1418@gmail.com>
2020-12-22 14:25:05 +0100

    add rspec test for enumerable method of each with index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -7,4 +7,14 @@ describe Enumerable do
     expect(enumerable.my_each).to eql()
     end
   end
+
+  describe Enumerable do
+    describe my_each_with_index do
+      it 'returns each item with their index positions in an array'
+      enumerable = Enumerable.new
+      expect(enumerable.my_each_with_index).to eql()
+      end
+    end
+
+
 end
