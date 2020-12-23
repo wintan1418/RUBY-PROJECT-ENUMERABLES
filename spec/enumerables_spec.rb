cc185b08e126318756b2bd9c15fbd488172b48af
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-23 14:16:30 +0500

    Complete my_each method RSpec 3 test cases

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,28 +1,27 @@
-require_relative './script.rb'
+require_relative '../script.rb'
 
 describe Enumerable do
-  describe my_each do
-    it 'returns each item in a array'
-    enumerable = Enumerable.new
-    expect(enumerable.my_each).to eql()
-    end
+  int_arr = [1, 3, 5]
+  range_arr = (1..6)
+  string_arr = ['This', 'is', 'a string', 'array']
+
+  it 'returns each item in a array' do
+    expect(string_arr.my_each {|x| x}).to eql(string_arr.each {|x| x})
+  end
+  
+  it 'returns each item in a array' do
+    expect(string_arr.my_each {|x| x}).to eql(string_arr.each {|x| x})
   end
 
-  describe Enumerable do
-    describe my_each_with_index do
-      it 'returns each item with their index positions in an array'
-      enumerable = Enumerable.new
-      expect(enumerable.my_each_with_index).to eql()
-      end
-    end
+  it 'returns each item in a array' do
+    expect(string_arr.my_each {|x| x}).to eql(string_arr.each {|x| x})
+  end
 
-    describe Enumerable do
-      describe my_select do
-        it 'returns only the selected items in an array'
-        enumerable = Enumerable.new
-        expect(enumerable.my_select).to eql()
-        end
-      end
+  # it 'returns each item with index in a array' do
+    
+  #   actual = int_arr.my_each_with_index do |x|
 
-
+  #   end
+  #   expect().to eql(int_arr.each {|x|})
+  # end
 end
